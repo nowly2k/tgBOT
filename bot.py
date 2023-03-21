61a664621cd7ed35ceda9b4e624044d1cdb5288a
Marsel <nowly2k@mail.ru>
2023-03-21 15:13:29 +0300

    "все говорят: <повторяет текст>, а ты купи слона"

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import time
 
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = '5502666138:AAGN1bG1ThT10N-0j0wO-Eb-VLEE7WMltfY'
-TEXT: str = 'Ура! Классный апдейт!'
 MAX_COUNTER: int = 100
 
 offset: int = -2
@@ -22,7 +21,8 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            TEXT = result['message']['text']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text=Все говорят: "{TEXT}",\nА ты купи слона!')
 
     time.sleep(1)
     counter += 1
