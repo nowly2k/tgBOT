0175aa5e55bf776cb34b68f6e5d69a67e347eb59
Marsel <nowly2k@mail.ru>
2023-03-22 15:49:58 +0300

    отправляет рандомную картинку с котом

--- bot.py
+++ bot.py
@@ -3,17 +3,19 @@ import time
 
 
 API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://aws.random.cat/meow'
 BOT_TOKEN: str = '5502666138:AAGN1bG1ThT10N-0j0wO-Eb-VLEE7WMltfY'
-MAX_COUNTER: int = 100
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком, но все поломалось :('
 
 offset: int = -2
 counter: int = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
 
-while counter < MAX_COUNTER:
+while counter < 100:
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    print('attempt =', counter)
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
@@ -21,8 +23,11 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            TEXT = result['message']['text']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text=Все говорят: "{TEXT}",\nА ты купи слона!')
-
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()['file']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
     time.sleep(1)
     counter += 1
