533b6154d2b2832135134de73f716c43822b3621
Marsel <nowly2k@mail.ru>
2023-03-24 12:30:16 +0300

    копирует любое сообщение и отвечает

--- bot.py
+++ bot.py
@@ -1,33 +1,30 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+API_TOKEN: str = '5502666138:AAGN1bG1ThT10N-0j0wO-Eb-VLEE7WMltfY'
+
+bot: Bot = Bot(API_TOKEN)
+dp: Dispatcher = Dispatcher()
 
 
-API_URL: str = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://aws.random.cat/meow'
-BOT_TOKEN: str = '5502666138:AAGN1bG1ThT10N-0j0wO-Eb-VLEE7WMltfY'
-ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком, но все поломалось :('
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь.')
 
-offset: int = -2
-counter: int = 0
-cat_response: requests.Response
-cat_link: str
+async def process_help_command(message: Message):
+    await message.answer('Напиши что нибудь мне, а в ответ я пришлю твое сообщение.')
 
+    
+async def process_message(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Ошибка')
+    
+    
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(process_message)
 
-while counter < 100:
-
-    print('attempt =', counter)
-
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()['file']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-    time.sleep(1)
-    counter += 1
+if __name__ == '__main__':
+    dp.run_polling(bot)
